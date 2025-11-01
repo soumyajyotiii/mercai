934c44b037a51b0e0a715d517d61e4dfd17e7a98
soumyajyotiii <soumyajyoti.1999@gmail.com>
2025-11-01 11:29:28 +0530

    add terraform provider and variables configuration
    setup aws provider with terraform 5.x
    define variables for vpc, ecs, and deployment settings

---
+++ terraform/variables.tf
@@ -0,0 +1,59 @@
+variable "aws_region" {
+  description = "aws region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "project name for resource naming"
+  type        = string
+  default     = "ecs-bg-deploy"
+}
+
+variable "environment" {
+  description = "environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "vpc_cidr" {
+  description = "cidr block for vpc"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "container_port" {
+  description = "port exposed by the container"
+  type        = number
+  default     = 3000
+}
+
+variable "app_count" {
+  description = "number of docker containers to run"
+  type        = number
+  default     = 2
+}
+
+variable "fargate_cpu" {
+  description = "fargate instance cpu units"
+  type        = string
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "fargate instance memory"
+  type        = string
+  default     = "512"
+}
+
+variable "app_image" {
+  description = "docker image to run in ecs cluster"
+  type        = string
+  default     = "nginx:latest"
+}
